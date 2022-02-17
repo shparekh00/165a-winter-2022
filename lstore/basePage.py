959c2052acf1214cac93f5db881ece1432351a32
Soumya Duriseti <soumyaduriseti@gmail.com>
2022-02-16 22:57:18 -0800

    attempted to make a setter for base_page_copy

--- lstore/basePage.py
+++ lstore/basePage.py
@@ -10,7 +10,21 @@ class basePage(virtualPage):
       self.num_updates = 0 # reset to 0 after each merge
       self.tps = 0 # contains latest tail_rid of merge
       self.base_page_copy = 0 # will contain a base page after merge, otherwise it will be 0
-      pass
+      #self._callbacks = []
+
+   @property
+   def base_page_copy(self):
+        return self.base_page_copy
+
+   @base_page_copy.setter
+   def base_page_copy(self, new_value):
+        old_value = self.base_page_copy
+        self.base_page_copy = new_value
+        #stop main thread
+        #basepage = copy
+        #https://stackoverflow.com/questions/51885246/callback-on-variable-change-in-python
+
 
    def copy(self):
       return copy.deepcopy(self)
+
